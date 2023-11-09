f26e8b3ffa7e9c7851481a7cb4cacce543aa4137
Soumya Nasipuri <nasipurisoumya@gmail.com>
2023-11-09 17:08:42 +0530

    Create JSON response with basic Django, movie list and details
        - create a movie list
        - create urls and views
        - get JSON response with basic Django

--- movieFlex/settings.py
+++ movieFlex/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'wathclist_app',
+    'watchlist_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    year = models.IntegerField()
+    active = models.BooleanField(default=True)
+    
+    def __str__(self) -> str:
+        return self.title
